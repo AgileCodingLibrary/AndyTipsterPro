979ac51a124c7e99e9f6a291fd25d95ddbcb8fe0
faz ahmed <fazahmed20xx@hotmail.com>
2020-01-18 11:38:43 +0000

    Added space holder Tips views and Action methods.

--- EmployeeManagement/Controllers/HomeController.cs
+++ EmployeeManagement/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace AndyTipsterPro.Controllers
 
         public HomeController(IEmployeeRepository employeeRepository,
                               IHostingEnvironment hostingEnvironment,
-                              ILogger<HomeController> logger, 
+                              ILogger<HomeController> logger,
                               AppDbContext context)
         {
             _employeeRepository = employeeRepository;
@@ -29,14 +29,6 @@ namespace AndyTipsterPro.Controllers
         [AllowAnonymous]
         public ViewResult Index()
         {
-            //var model = _employeeRepository.GetAllEmployee()
-            //                .Select(e =>
-            //                {
-            //                    e.EncryptedId = protector.Protect(e.Id.ToString());
-            //                    return e;
-            //                });
-
-            
             return View();
         }
 
@@ -52,7 +44,7 @@ namespace AndyTipsterPro.Controllers
             return View("Index");
         }
 
-            
+
 
         [AllowAnonymous]
         public ViewResult Subscribe()
@@ -64,7 +56,7 @@ namespace AndyTipsterPro.Controllers
         public ViewResult Faq()
         {
             var model = _context.Questions.ToList();
-            
+
             return View(model);
         }
         [AllowAnonymous]
@@ -73,123 +65,31 @@ namespace AndyTipsterPro.Controllers
             return View();
         }
 
-        //[AllowAnonymous]
-        //public ViewResult Details(string id)
-        //{
-        //    //throw new Exception("Error in Details View");
 
-        //    logger.LogTrace("Trace Log");
-        //    logger.LogDebug("Debug Log");
-        //    logger.LogInformation("Information Log");
-        //    logger.LogWarning("Warning Log");
-        //    logger.LogError("Error Log");
-        //    logger.LogCritical("Critical Log");
+        [Authorize]
+        public ActionResult AndyTipsterTips()
+        {
+            var model = _context.Tips.FirstOrDefault();
+            return View(model);
+        }
 
-        //    int employeeId = Convert.ToInt32(protector.Unprotect(id));
 
-        //    Employee employee = _employeeRepository.GetEmployee(employeeId);
+        [Authorize]
+        public ActionResult IrishRacingTips()
+        {
+            var model = _context.Tips.FirstOrDefault();
+            return View(model);
+        }
 
-        //    if (employee == null)
-        //    {
-        //        Response.StatusCode = 404;
-        //        return View("EmployeeNotFound", employeeId);
-        //    }
+        [Authorize]
+        public ActionResult UltimatePackTips()
+        {
+            var model = _context.Tips.FirstOrDefault();
+            return View(model);
+        }
 
-        //    HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
-        //    {
-        //        Employee = employee,
-        //        PageTitle = "Employee Details"
-        //    };
 
-        //    return View(homeDetailsViewModel);
-        //}
-
-        //[HttpGet]
-        //public ViewResult Create()
-        //{
-        //    return View();
-        //}
-
-        //[HttpGet]
-        //public ViewResult Edit(int id)
-        //{
-        //    Employee employee = _employeeRepository.GetEmployee(id);
-        //    EmployeeEditViewModel employeeEditViewModel = new EmployeeEditViewModel
-        //    {
-        //        Id = employee.Id,
-        //        Name = employee.Name,
-        //        Email = employee.Email,
-        //        Department = employee.Department,
-        //        ExistingPhotoPath = employee.PhotoPath
-        //    };
-        //    return View(employeeEditViewModel);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Edit(EmployeeEditViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        Employee employee = _employeeRepository.GetEmployee(model.Id);
-        //        employee.Name = model.Name;
-        //        employee.Email = model.Email;
-        //        employee.Department = model.Department;
-        //        if (model.Photo != null)
-        //        {
-        //            if (model.ExistingPhotoPath != null)
-        //            {
-        //                string filePath = Path.Combine(hostingEnvironment.WebRootPath,
-        //                    "images", model.ExistingPhotoPath);
-        //                System.IO.File.Delete(filePath);
-        //            }
-        //            employee.PhotoPath = ProcessUploadedFile(model);
-
-        //        }
-
-        //        _employeeRepository.Update(employee);
-        //        return RedirectToAction("index");
-        //    }
-
-        //    return View();
-        //}
-
-        //private string ProcessUploadedFile(EmployeeCreateViewModel model)
-        //{
-        //    string uniqueFileName = null;
-        //    if (model.Photo != null)
-        //    {
-        //        string uploadsFolder = Path.Combine(hostingEnvironment.WebRootPath, "images");
-        //        uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Photo.FileName;
-        //        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-        //        using (var fileStream = new FileStream(filePath, FileMode.Create))
-        //        {
-        //            model.Photo.CopyTo(fileStream);
-        //        }
-        //    }
-
-        //    return uniqueFileName;
-        //}
-
-        //[HttpPost]
-        //public IActionResult Create(EmployeeCreateViewModel model)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        string uniqueFileName = ProcessUploadedFile(model);
-
-        //        Employee newEmployee = new Employee
-        //        {
-        //            Name = model.Name,
-        //            Email = model.Email,
-        //            Department = model.Department,
-        //            PhotoPath = uniqueFileName
-        //        };
-
-        //        _employeeRepository.Add(newEmployee);
-        //        return RedirectToAction("details", new { id = newEmployee.Id });
-        //    }
-
-        //    return View();
-        //}
     }
+
+
 }
